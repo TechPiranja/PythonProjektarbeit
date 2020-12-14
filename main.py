a13050590231cc9678a03fce9d633a55e295c093
Piranja <techpiranja@gmail.com>
2020-12-14 17:36:25 +0100

    ! fixed noType error
    grid returns null!

--- main.py
+++ main.py
@@ -12,12 +12,55 @@ frame1 = Frame(root)
 frame2 = Frame(root)
 pt = Table(frame2)
 
+h1 = Label(root, text="Datendateien", bg="#eee")
+h1.pack(padx=5, pady=5, fill="x")
+
 def openFileDialog():
     files = list(askopenfilenames(parent=root, title='Choose a file'))
     updateDf(files)
 
+def deleteSelectedFiles():
+    selectedFiles.delete(1.0, END)
+
+def getSelectedFiles():
+    return selectedFiles.get(1.0, END)
+
+#Dialog Frame
+dialogFrame = Frame(root)
+
+open_btn = Button(dialogFrame, text="Datendateien öffnen", command=openFileDialog, width=20, padx=0)
+open_btn.grid(row=0, column=0)
+deleteAll_btn = Button(dialogFrame, text="Alle entfernen", command=deleteSelectedFiles, width=20, padx=0)
+deleteAll_btn.grid(row=1, column=0)
+selectedFiles = Text(dialogFrame, height=4, borderwidth=2, relief=SUNKEN)
+selectedFiles.grid(row=0, column=1, rowspan=2)
+dialogFrame.pack(fill="x", padx=5)
+
+#Detector Frame
+h1 = Label(root, text="Detector", bg="#eee")
+h1.pack(padx=5, pady=5, fill="x")
+
+detectorFrame = Frame(root)
+
+Label(detectorFrame, text="CSV-Zeichenkodierung:", width=20, anchor="w", justify="left", padx=0).grid(row=0, column=0)
+encodingText = Text(detectorFrame, height=1, borderwidth=2, relief=SUNKEN)
+encodingText.grid(row=0, column=1)
+
+Label(detectorFrame, text="Has Header:", width=20, anchor="w", justify="left", padx=0).grid(row=1, column=0)
+hasHeaderText = Text(detectorFrame, height=1, borderwidth=2, relief=SUNKEN)
+hasHeaderText.grid(row=1, column=1)
+
+detectorFrame.pack(fill="x", padx=5, pady=5)
+
+#Vorschau und Pandastable
+vorschau = Label(root, text="Vorschau", bg="#eee")
+vorschau.pack(expand=TRUE, fill="x", padx=5, side=TOP)
+pt.show()
+frame2.pack(pady=10, padx=5, fill="both", side=TOP)
+
 def updateDf(files: list):
     # TODO use CSV Merge Method ( not implemented yet )
+    print(files)
     selectedFiles.insert(END, files)  # TODO: import without {} brackets
     encodingText.insert(1.0, detector.detectEncoding(files[0]))
 
@@ -26,43 +69,6 @@ def updateDf(files: list):
     print("has Header: " + str(detector.detectHeader(files[0])))
     pt.updateModel(TableModel(updatedDataframe))
     pt.redraw()
-
-def deleteSelectedFiles():
-    selectedFiles.delete(1.0, END)
-
-def getSelectedFiles():
-    return selectedFiles.get(1.0, END)
-
-
-h1 = Label(frame1, text="Datendateien", bg="#eee")
-h1.pack(expand=TRUE, fill="x")
-
-#Dialog Frame
-dialogFrame = Frame(root)
-
-open_btn = Button(dialogFrame, text="Datendateien öffnen", command=openFileDialog, width=20, padx=0).grid(row=0, column=0)
-deleteAll_btn = Button(dialogFrame, text="Alle entfernen", command=deleteSelectedFiles, width=20, padx=0).grid(row=1, column=0)
-
-selectedFiles = Text(dialogFrame, height=4, borderwidth=2, relief=SUNKEN).grid(row=0, column=1, rowspan=2)
-
-dialogFrame.pack(fill="x", padx=5)
-
-#Detector Frame
-detectorFrame = Frame(root)
-
-encoding = Label(detectorFrame, text="CSV-Zeichenkodierung:", width=20, anchor="w", justify="left", padx=0).grid(row=0, column=0)
-encodingText = Text(detectorFrame, height=1, borderwidth=2, relief=SUNKEN).grid(row=0, column=1)
-
-hasHeaderLabel = Label(detectorFrame, text="Has Header:", width=20, anchor="w", justify="left", padx=0).grid(row=1, column=0)
-hasHeaderText = Text(detectorFrame, height=1, borderwidth=2, relief=SUNKEN).grid(row=1, column=1)
-
-detectorFrame.pack(fill="x", padx=5)
-
-#Vorschau und Pandastable
-vorschau = Label(root, text="Vorschau", bg="#eee")
-vorschau.pack(expand=TRUE, fill="x", padx=5, side=TOP)
-pt.show()
-frame2.pack(pady=10, padx=5, fill="both", side=TOP)
 
 if __name__ == '__main__':
     root.mainloop()
