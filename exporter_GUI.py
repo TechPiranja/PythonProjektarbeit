56cc335f12c6211dabefe8865e5469c8d121af92
Piranja <techpiranja@gmail.com>
2020-12-21 18:29:26 +0100

    + exporter can now export into csv and xml

--- exporter_GUI.py
+++ exporter_GUI.py
@@ -1,7 +1,6 @@
 from functools import partial
 from tkinter import END, Tk, Toplevel, Label, Frame, Text, SUNKEN, Button
 from tkinter.filedialog import askdirectory
-from pandastable import Table
 import exporter
 
 
@@ -9,13 +8,18 @@ def openFolderDialog(window, folderPath):
     files = askdirectory(parent=window, title='Choose a file')
     folderPath.insert(END, files)
 
-def prepareExport(folderPathText, fileNameText, pt: Table):
+def prepareExport(folderPathText, fileNameText, importer, dialect, isCSV = True):
+    dataframe = importer.getDataFrame()
     folderPath = folderPathText.get("1.0", "end-1c")
     fileName = fileNameText.get("1.0", "end-1c")
     filePath = folderPath + "/" + fileName
-    exporter.exportToCSV(filePath, pt)
+    if (isCSV):
+        exporter.exportToCSV(filePath, dataframe, dialect)
+    else:
+        exporter.exportToXML(filePath, dataframe, dialect)
 
-def initExportDialog(root: Tk, pt: Table):
+def initExportDialog(root: Tk, importer, dialect):
+    importer = importer
     window = Toplevel(root)
     h1 = Label(window, text="Tabelle exportieren", bg="#eee")
     h1.pack(padx=5, pady=5, fill="x")
@@ -29,8 +33,10 @@ def initExportDialog(root: Tk, pt: Table):
     Label(folderFrame, text="File Name:", width=20, anchor="w", justify="left", padx=0).grid(row=1, column=0)
     fileNameText = Text(folderFrame, height=1, borderwidth=2, relief=SUNKEN)
     fileNameText.grid(row=1, column=1)
-    fileNameText.insert(1.0, "filename.csv")
+    fileNameText.insert(1.0, "filename")
 
+    exportCSVBtn = Button(folderFrame, text="Export to CSV", command=partial(prepareExport, folderPathText, fileNameText, importer, dialect), width=20, padx=0)
+    exportCSVBtn.grid(row=2, column=0)
+    exportXMLBtn = Button(folderFrame, text="Export to XML", command=partial(prepareExport, folderPathText, fileNameText, importer, dialect, False), width=20, padx=0, anchor="e")
+    exportXMLBtn.grid(row=2, column=1)
     folderFrame.pack(padx=5)
-    exportBtn = Button(window, text="Exportieren", command=partial(prepareExport, folderPathText, fileNameText, pt), width=20, padx=0)
-    exportBtn.pack(fill="x", padx=10, pady=10)
