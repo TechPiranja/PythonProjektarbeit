672e063bc1d6394a008842622c0d3a8d92e5beed
Piranja <techpiranja@gmail.com>
2020-12-21 18:28:39 +0100

    + can now export to xml

--- exporter.py
+++ exporter.py
@@ -1,4 +1,19 @@
-from pandastable import Table
+from lxml import etree
 
-def exportToCSV(filePath: str, pt: Table):
-    pt.doExport(filePath)
+
+def exportToCSV(filePath: str, dataframe, dialect):
+    dataframe.to_csv(filePath + ".csv", encoding=dialect.encoding, sep=dialect.delimiter, quotechar=dialect.quotechar)
+    #TODO: Message that the File was exported successfully!
+
+def exportToXML(filePath: str, dataframe, dialect):
+    root = etree.Element('items')
+
+    for i, row in dataframe.iterrows():
+        item = etree.SubElement(root, 'item')
+
+        for field in row.index:
+            fieldElement = etree.SubElement(item, field)
+            fieldElement.text = str(row[field])
+
+    document = etree.ElementTree(root)
+    document.write(filePath + ".xml", xml_declaration=True, pretty_print=True, encoding=dialect.encoding)
