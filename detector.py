ad897c26ea3117271c10255295a753566ad167ea
Piranja <techpiranja@gmail.com>
2021-01-12 13:01:44 +0100

    + written docstrings for detector.py and exporter.py

--- detector.py
+++ detector.py
@@ -5,12 +5,27 @@ import pandas
 
 
 class Dialect:
+    """
+    The Dialect class can guess the dialect from a csv or xml file. It is also able to detect the Encoding of a file,
+    check its header, and fill the header with correct header names if there is none.
+
+    Attributes:
+         encoding: pre-defined default encoding
+        hasHeader: pre-defined default hasHeader
+        delimiter: pre-defined default delimiter
+        quoteChar: pre-defined default quoteChar
+    """
     encoding: str = "utf-8"
     hasHeader: bool = False
     delimiter: str = ","
     quoteChar: str = "\""
 
     def guessDialectCSV(self, filePath: str):
+        """
+        This method guesses the dialect from a csv file
+
+        :param filePath: the csv filepath which will be sniffed in order to get the dialect data
+        """
         self.encoding = self.detectEncoding(filePath)
 
         with open(filePath, 'r') as rawdata:
@@ -20,25 +35,53 @@ class Dialect:
             self.delimiter = self.getSniffer(data).delimiter
 
     def guessDialectXML(self, data: str):
+        """
+        This method guesses the dialect from a xml file
+
+        :param data: the data from the read etree.pass method
+        """
         self.quotechar = self.getSniffer(data).quotechar
         self.delimiter = self.getSniffer(data).delimiter
         self.hasHeader = self.checkHasHeader(data)
 
     def detectEncoding(self, filePath: str):
+        """
+        Detect the encoding of the given file
+
+        :param filePath: the given filepath which will be used to detec the encoding
+        :return: the detected encoding of the file
+        """
         with open(filePath, 'rb') as rawdata:
             data = rawdata.read()
         result = chardet.detect(data)
         return result["encoding"]
 
     def checkHasHeader(self, data: str):
+        """
+        Uses the csv sniffer to detect if the given data contains a header
+
+        :param data: the data which is read from the rawdata.read
+        :return: boolean if the data contains a header
+        """
         return csv.Sniffer().has_header(data)
 
     def getSniffer(self, data: str):
+        """
+        gets the sniffer from the csv sniffer from the given data
+
+        :param data: the data which is read from the rawdata.read
+        :return: the csv sniffer from the given data
+        """
         return csv.Sniffer().sniff(data)
 
 
-# guesses Header Names based on Type which is checked with Regex Match
 def guessHeaderNames(dataFrame: pandas.DataFrame):
+    """
+    guesses header names based on the types in each column which is then checked with regex match
+
+    :param dataFrame: the dataframe from the importer_gui
+    :return: the new header column
+    """
     headerNameCount = {}
     newHeaders = {}
 
--- exporter.py
+++ exporter.py
@@ -2,10 +2,24 @@ from lxml import etree
 from tkinter import messagebox
 
 def exportToCSV(filePath: str, dataframe, dialect):
+    """
+    Helper method which exports the dataframe from the importer_gui preview into a csv file with the given dialect.
+
+    :param filePath: The file destination for the exported csv file
+    :param dataframe: the dataframe from the importer_gui preview which will be converted into a csv file
+    :param dialect: the dialect which will be used to convert the dataframe into a csv file
+    """
     dataframe.to_csv(filePath + ".csv", encoding=dialect.encoding, sep=dialect.delimiter, quotechar=dialect.quoteChar, index=False)
     messagebox.showinfo(title="Success!", message="The Export was successful!")
 
 def exportToXML(filePath: str, dataframe, dialect):
+    """
+    Helper method which export the dataframe from the importer_gui preview into a xml file with the given dialect.
+
+    :param filePath: The file destination for the exported xml file
+    :param dataframe: the dataframe from the importer_gui preview which will be converted into a xml file
+    :param dialect: the dialect which will be used to convert the dataframe into a xml file
+    """
     root = etree.Element('items')
 
     for i, row in dataframe.iterrows():
@@ -17,3 +31,4 @@ def exportToXML(filePath: str, dataframe, dialect):
 
     document = etree.ElementTree(root)
     document.write(filePath + ".xml", xml_declaration=True, pretty_print=True, encoding=dialect.encoding)
+    messagebox.showinfo(title="Success!", message="The Export was successful!")
